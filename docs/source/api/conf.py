cf0bf890bdcace6a8ff248cef58471a84c8a0250
gauribhoite <gauribhoite@users.noreply.github.com>
2015-09-29 15:21:49 +0100

    added google appengine in  requirements.txt

--- docs/source/api/conf.py
+++ docs/source/api/conf.py
@@ -20,7 +20,7 @@ import shlex
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath('../../../app/'))
-sys.path.insert(0, os.path.abspath('../../../env/google_appengine/lib/yaml/lib/'))
+sys.path.insert(1, os.path.abspath('../../../env/google_appengine/lib/yaml/lib/'))
 
 
 
