f4882c9035e3863de752035bdd841b999213eaa5
gauribhoite <gauribhoite@users.noreply.github.com>
2015-09-29 18:01:12 +0100

    checged conf.py to working path

--- docs/source/api/conf.py
+++ docs/source/api/conf.py
@@ -19,8 +19,8 @@ import shlex
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.insert(0, os.path.abspath('../../../../app/'))
-sys.path.insert(1, os.path.abspath('../../../../env/google_appengine/lib/yaml/lib/'))
+sys.path.insert(0, os.path.abspath('../../../app/'))
+sys.path.insert(1, os.path.abspath('../../../env/google_appengine/lib/yaml/lib/'))
 
 
 
